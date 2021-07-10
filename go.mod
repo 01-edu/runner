41c668287dcbe2aee1a28fecdd6defe3ebdca0cd
xpetit <32063953+xpetit@users.noreply.github.com>
2021-07-10 19:12:19 +0200

    Add script

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/01-edu/test-runner
+module main
 
 go 1.16
 
---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS='
+'
+cd -P "$(dirname "$0")"
+
+docker build -t test-runner .
+docker container rm --force test-runner 2>/dev/null
+docker run \
+    --detach \
+    --name test-runner \
+    --network https \
+    --publish 8080:8080 \
+    --env REGISTRY_PASSWORD \
+    --restart unless-stopped \
+    --volume /var/run/docker.sock:/var/run/docker.sock:ro \
+    --label org.01-edu.https=test-runner.01-edu.org:8080 \
+    test-runner
